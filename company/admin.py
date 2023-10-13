1e66babd966587c6a263012fb5912590aa90639d
tmsnvl95 <tmsnvl95@naver.com>
2023-10-13 12:42:37 +0900

    feat(admin): Django 어드민 사이트 설정 및 모델 등록
    - Django 어드민 사이트 초기 설정을 구성
    - 어드민 접근을 위한 슈퍼유저 계정을 생성
    - Company, Applicant를 Django 어드민에 등록

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Company  # 이 앱의 models.py에서 Company 모델을 import 합니다.
 
-# Register your models here.
+admin.site.register(Company)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Applicant  # 이 앱의 models.py에서 Applicant 모델을 import 합니다.
 
-# Register your models here.
+admin.site.register(Applicant)
