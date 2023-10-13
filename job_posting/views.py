fec9c4f77bca01be17dd1f3c311c4ec551831395
tmsnvl95 <tmsnvl95@naver.com>
2023-10-13 15:22:35 +0900

    feat(job_posting): 채용공고 등록 기능 추가
    - 채용공고를 등록하는 views.py 로직 완성
    ㄴjson 형태 요청을 처리하도록 form-dataq형태 코드를 수정
    - 관련 URL 및 모델폼 구현

--- job_posting/views.py
+++ job_posting/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import JobPostingForm
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def create_job_posting(request):
+    try:
+        data = json.loads(request.body) # 파싱된 JSON 데이터
+        form = JobPostingForm(data)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Job posting created successfully.")
+        else:
+            return JsonResponse(form.errors, status=400)
+    except json.JSONDecodeError:
+        return HttpResponse("Error: Submitted data is not in JSON format", status=400)
