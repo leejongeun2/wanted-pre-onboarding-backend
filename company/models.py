09f50952204b1472f8468528e175656c6bfcab05
tmsnvl95 <tmsnvl95@naver.com>
2023-10-14 01:18:08 +0900

    feat(company): Company 모델에 location, country 필드추가

--- company/models.py
+++ company/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    country = models.CharField(max_length=100)  # 국가 필드 추가
+    location = models.CharField(max_length=100)  # 지역 필드 추가
 
     def __str__(self):
         return self.name
